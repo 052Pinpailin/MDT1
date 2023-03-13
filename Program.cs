d1e6c562685477748e45aeb3d7e4da4df61e7919
Pinpailin <pinpailin3146@gmail.com>
2023-03-13 17:35:09 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿class Program {
+    static void Main(string[] args) {
+        Console.Write("Please input One: ");
+        int One = int.Parse(Console.ReadLine());
+        Console.Write("Please input w1: ");
+        int w1 = int.Parse(Console.ReadLine());
+        Console.Write("Please input h1: ");
+        int h1 = int.Parse(Console.ReadLine());
+       
+        Console.Write("Please input Two: ");
+        int Two = int.Parse(Console.ReadLine());
+        Console.Write("Please input w2: ");
+        int w2 = int.Parse(Console.ReadLine());
+        Console.Write("Please input h2: ");
+        int h2 = int.Parse(Console.ReadLine());
+
+        if ((w1 >= 10 && w1 <= 9) ){
+            Console.WriteLine("Too Much Overlaping");
+        }else {
+            Console.WriteLine("Not Much Overlaping");
+        }
+        
+        if ((h1 >= 10 && h1 <= 9) );
+
+        if ((w2 >= 5 ));
+
+        if ((h2 >= 15 ));
+    }
+}
+        
